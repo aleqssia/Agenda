22b5b4d9678c5f2c727de48d678969132c4af8bd
Alexia <alexiarmarques.ti@gmail.com>
2019-09-19 15:14:28 -0300

    Adicionando projeto no repositorio

---
+++ ContForm/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace ContForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.ListBox();
+            this.lblNum = new System.Windows.Forms.ListBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(11, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "E-mail:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(71, 84);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(244, 24);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(71, 50);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(340, 24);
+            this.txtName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(11, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbTipo);
+            this.groupBox1.Controls.Add(this.btnRemove);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtNum);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblTipo);
+            this.groupBox1.Controls.Add(this.lblNum);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Location = new System.Drawing.Point(16, 114);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(395, 175);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fones:";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.Enabled = false;
+            this.cmbTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Residencial",
+            "Comercial",
+            "Celular"});
+            this.cmbTipo.Location = new System.Drawing.Point(275, 97);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(89, 26);
+            this.cmbTipo.TabIndex = 11;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRemove.Location = new System.Drawing.Point(267, 141);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(115, 28);
+            this.btnRemove.TabIndex = 10;
+            this.btnRemove.Text = "Remover";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAdd.Location = new System.Drawing.Point(25, 141);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 28);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(237, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tipo";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.Location = new System.Drawing.Point(74, 97);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(101, 24);
+            this.txtNum.TabIndex = 6;
+            this.txtNum.TextChanged += new System.EventHandler(this.textBoxNum_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(51, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nº";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.FormattingEnabled = true;
+            this.lblTipo.ItemHeight = 18;
+            this.lblTipo.Location = new System.Drawing.Point(241, 23);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(123, 58);
+            this.lblTipo.TabIndex = 1;
+            // 
+            // lblNum
+            // 
+            this.lblNum.FormattingEnabled = true;
+            this.lblNum.ItemHeight = 18;
+            this.lblNum.Location = new System.Drawing.Point(55, 23);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(120, 58);
+            this.lblNum.TabIndex = 0;
+            this.lblNum.SelectedIndexChanged += new System.EventHandler(this.ListBoxNum_SelectedIndexChanged);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(30, 306);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(62, 23);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "Novo";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(187, 306);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(65, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Gravar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(338, 306);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(60, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Excluir";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(321, 84);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Pesquisar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(147, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 33);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "AGENDA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(453, 347);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Agenda";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lblTipo;
+        private System.Windows.Forms.ListBox lblNum;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ContForm/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace ContForm
+{
+    public partial class Form1 : Form
+    {
+        Contatos contatos = new Contatos();
+        List<string> nums = new List<string>();
+        List<string> tipo = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbTipo.SelectedIndex = 0;
+        }
+
+        public void listaDeNumeros()
+        {
+            lblNum.Items.Clear();
+            lblTipo.Items.Clear();
+
+            for (int i = 0; i < this.nums.Count; i++)
+            {
+                lblNum.Items.Add(this.nums[i]);
+                lblTipo.Items.Add(this.tipo[i]);
+            }
+        }
+
+        public void limparForm()
+        {
+            txtEmail.Text = "";
+            txtName.Text = "";
+            txtNum.Text = "";
+            cmbTipo.SelectedIndex = 0;
+            lblNum.Items.Clear();
+            lblTipo.Items.Clear();
+            this.nums.Clear();
+            this.tipo.Clear();
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            if(txtEmail.Text != "" || txtName.Text != "" || txtNum.Text != "")
+            {
+                result = MessageBox.Show("Tem certeza que deseja limpar o formulário?","", buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    limparForm();
+                }
+            }
+            
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if(txtEmail.Text != "")
+            {
+                List<Fone> teladd = new List<Fone>();
+                for (int i = 0; i < this.nums.Count; i++)
+                {
+                    Fone newtel = new Fone(this.nums[i], this.tipo[i]);
+                    teladd.Add(newtel);
+                }
+
+                contatos.Adicionar(new Contato(txtEmail.Text, txtName.Text, teladd));
+
+                MessageBox.Show("Contato salvo");
+            }
+            else
+            {
+                MessageBox.Show("ERRO: Campo E-mail não pode estar em branco");
+            }
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            
+            string emailpesquisa = txtEmail.Text;
+            Contato contatoPesquisa = contatos.Pesquisar(new Contato(emailpesquisa));
+            if (contatoPesquisa.Email == emailpesquisa)
+            {
+                txtEmail.Text = contatoPesquisa.Email;
+                txtName.Text = contatoPesquisa.Nome;
+
+                this.nums.Clear();
+                this.tipo.Clear();
+
+                if(txtEmail.Text != "")
+                {
+                    for (int i = 0; i < contatoPesquisa.Telefones.Count; i++)
+                    {
+                        this.nums.Add(contatoPesquisa.Telefones[i].Numero);
+                        this.tipo.Add(contatoPesquisa.Telefones[i].Tipo);
+                    }
+                }
+                
+
+                listaDeNumeros();
+            }
+
+            else
+                MessageBox.Show("Contato não encontrado");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string emailexclui = txtEmail.Text;
+            contatos.Remover(new Contato(emailexclui));
+            MessageBox.Show("Usuário removido");
+            limparForm();
+        }
+
+        private void textBoxNum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            this.nums.Add(txtNum.Text);
+            this.tipo.Add(cmbTipo.Text);
+
+            listaDeNumeros();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            this.tipo.RemoveAt(this.nums.IndexOf(txtNum.Text));
+            this.nums.Remove(txtNum.Text);
+            
+
+            listaDeNumeros();
+        }
+
+        private void ListBoxNum_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblTipo.SelectedIndex = lblNum.SelectedIndex;
+            txtNum.Text = lblNum.SelectedItem.ToString();
+            cmbTipo.Text = lblTipo.Items[lblNum.SelectedIndex].ToString();
+        }
+    }
+}
